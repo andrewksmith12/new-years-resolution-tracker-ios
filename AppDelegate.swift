769592eb8df1c0def3f36d1f01618f71adb1c480
Andrew Smith <andrew@andrewksmith.net>
2021-01-09 15:57:17 -0600

    first commit

---
+++ AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Resolutte
+//
+//  Created by Andrew Smith on 1/9/21.
+//
+
+import UIKit
+import Firebase
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+      [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
