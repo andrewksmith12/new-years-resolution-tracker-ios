e5a0777232703b149a9295c32375947c788c1c90
Andrew Smith <andrew@andrewksmith.net>
2021-01-09 15:12:27 -0600

    Initial Commit

---
+++ Resolutte/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Resolutte
+//
+//  Created by Andrew Smith on 1/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Resolutte/ResolutteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResolutteApp.swift
+//  Resolutte
+//
+//  Created by Andrew Smith on 1/9/21.
+//
+
+import SwiftUI
+
+@main
+struct ResolutteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
